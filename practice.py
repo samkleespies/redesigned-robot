bb001beedfa63231b6fca87a3eeb10271ef70e01
Navon Francis <navonfrancis@gmail.com>
2017-10-24 01:32:02 -0400

    added actual price change

--- practice.py
+++ practice.py
@@ -8,14 +8,18 @@ sauce = urllib.request.urlopen('http://www.investopedia.com/markets/stocks/baba/
 # pass the HTML code to BeautifulSoup
 soup = bs.BeautifulSoup(sauce, 'lxml')
 baba = float(soup.td.string)
-flux = soup.find('td', attrs={'class': 'value-change'}).string
+flux = list(soup.find('td', attrs={'class': 'value-change'}).string)
+net_change = ""
+i = 0
 
-print("Current price of BABA:", baba)
-print("Yeserday's price of BABA:")
-print("Change since yesterday:", flux)
-# # calculate the price current share has increased/decreased
-# fluc = # (currPrice)(changeSinceYesterday)
-# price_yesterday =
+while flux[i] != ' ':
+    net_change += flux[i]
+    i += 1;
+
+print("Current price of BABA:   ", baba)
+print("Yeserday's price of BABA:", baba + float(net_change))
+print("Change since yesterday:  ", soup.find('td', attrs={'class': 'value-change'}).string)
+
 
 # grab the sauce bois, and read it
 sauce2 = urllib.request.urlopen('http://www.investopedia.com/markets/stocks/momo/').read()
@@ -23,7 +27,7 @@ sauce2 = urllib.request.urlopen('http://www.investopedia.com/markets/stocks/momo
 # pass the HTML code to BeautifulSoup
 soup2 = bs.BeautifulSoup(sauce2, 'lxml')
 
-print("Current price of MOMO:" + soup2.td.string)
+print("Current price of MOMO:    " + soup2.td.string)
 
 # grab the sauce bois, and read it
 sauce3 = urllib.request.urlopen('http://www.investopedia.com/markets/stocks/AMZN/').read()
@@ -31,4 +35,4 @@ sauce3 = urllib.request.urlopen('http://www.investopedia.com/markets/stocks/AMZN
 # pass the HTML code to BeautifulSoup
 soup3 = bs.BeautifulSoup(sauce3, 'lxml')
 
-print("Current price of AMZN:" + soup3.td.string)
+print("Current price of AMZN:    " + soup3.td.string)
